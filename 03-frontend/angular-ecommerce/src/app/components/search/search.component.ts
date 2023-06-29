89375ab6f7ff5de6ef2d08945f469d878f83b5a2
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-06-29 21:06:18 +0530

    added search functionality by name

---
+++ 03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor(private router: Router){
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  doSearchProduct(searchValue: string){
+    console.log("Search Value:" + searchValue);
+    this.router.navigateByUrl(`/search/${searchValue}`);
+  }
+
+}
