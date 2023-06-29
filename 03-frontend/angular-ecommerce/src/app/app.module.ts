e8a3eb859bdfce6e777bafd7edd1ef45092bce1c
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-06-30 00:55:41 +0530

    Added ProductDetails Component

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { NgOptimizedImage } from "@angular/common";
 import { Routes, RouterModule } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
   {path: 'search/:keyword', component: ProductListComponent },
   {path: 'category/:id', component: ProductListComponent },
   {path: 'category', component: ProductListComponent },
   {path: 'products', component: ProductListComponent },
+  {path: 'products/:id', component: ProductDetailsComponent },
   {path: '', redirectTo: '/products', pathMatch: 'full' },
   {path: '**', redirectTo: '/products', pathMatch: 'full' }
 ];
