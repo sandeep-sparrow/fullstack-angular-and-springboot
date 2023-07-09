aba43f53bf717ddacea99740ba781b80ee3a1597
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-07-09 23:44:58 +0530

    added CartService and added cart status compoent event data

--- 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   templateUrl: './cart-status.component.html',
   styleUrls: ['./cart-status.component.css']
 })
-export class CartStatusComponent {
+export class CartStatusComponent implements OnInit {
+
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    // subscribe for the events on the cartService
+    
+    // total price
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // total quantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+  }
 
 }
