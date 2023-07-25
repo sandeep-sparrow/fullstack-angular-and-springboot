8ddac1c9b6ad6fd97b345476191793c0911dd17c
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-07-25 16:52:59 +0530

    added checkout component & added checkout button to cart-details component html

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -14,9 +14,11 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 const routes: Routes = [
   {path: 'cart-details', component: CartDetailsComponent },
+  {path: 'checkout', component: CheckoutComponent},
   {path: 'products/:id', component: ProductDetailsComponent },
   {path: 'search/:keyword', component: ProductListComponent },
   {path: 'category/:id', component: ProductListComponent },
@@ -34,7 +36,8 @@ const routes: Routes = [
     ProductDetailsComponent,
     SearchComponent,
     CartStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
     imports: [
         RouterModule.forRoot(routes),
