b0d367b4e364477278aa5b7256313852cf7ccf39
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-07-25 17:12:15 +0530

    added reative checkoutFormGroup for customer using ReactiveFormModule

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: 'cart-details', component: CartDetailsComponent },
@@ -44,7 +45,8 @@ const routes: Routes = [
         BrowserModule,
         HttpClientModule,
         NgOptimizedImage,
-        NgbModule
+        NgbModule,
+        ReactiveFormsModule
     ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor(){}
+  // add support for reactive forms - app.modules.ts under imports sections
+  // define form here
+
+  checkoutFormGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
 
   ngOnInit(): void {
-    
+
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      })
+    });
   }
 
 }
