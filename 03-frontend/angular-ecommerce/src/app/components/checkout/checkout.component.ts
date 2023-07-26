fac0aab6c2c25be2ed44570e4422eba4db99eee2
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-07-26 10:28:31 +0530

    added luv2 shop form service to populate credit card months and years

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -16,7 +17,11 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder){}
+  creditCardMonths: number[] = [];
+  creditCardYears: number[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private luv2CodeFormService: Luv2ShopFormService){}
 
   ngOnInit(): void {
 
@@ -49,6 +54,25 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
+
+    // populate the credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("Current month: " + startMonth);
+
+    this.luv2CodeFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    )
+
+    // populate the credit card years
+    this.luv2CodeFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    )
   }
 
   copyShippingAddressToBillingAddress(event: Event){
