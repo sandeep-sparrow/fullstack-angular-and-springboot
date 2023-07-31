758806e8c20c6a8f4d340e4f60a3c8ab87371af9
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-07-31 22:59:40 +0530

    added state & country entity class for 02-backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -1,7 +1,6 @@
 package com.luv2code.ecommerce.entity;
 
 import jakarta.persistence.*;
-import jdk.jfr.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
