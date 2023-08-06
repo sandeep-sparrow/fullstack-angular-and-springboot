81e296b5e3918e23ffef6272511f822ec2dd0654
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-08-06 21:21:05 +0530

    added validations for credit Card form

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -72,10 +72,12 @@ export class CheckoutComponent implements OnInit {
                                       Luv2ShopValidators.notOnlyWhiteSpaces])
       }),
       creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
+        cardType: new FormControl('',[Validators.required]),
+        nameOnCard: new FormControl('',[Validators.required,
+                                        Validators.minLength(2),
+                                        Luv2ShopValidators.notOnlyWhiteSpaces]),
+        cardNumber: new FormControl('',[Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode: new FormControl('',[Validators.required, Validators.pattern('[0-9]{3}')]),
         expirationMonth: [''],
         expirationYear: ['']
       })
@@ -159,6 +161,22 @@ export class CheckoutComponent implements OnInit {
 
   get billingAddressZipCode(){
     return this.checkoutFormGroup.get('billingAddress.zipCode');
+  }
+
+  get creditCardType(){
+    return this.checkoutFormGroup.get('creditCard.cardType');
+  }
+
+  get creditCardNameOnCard(){
+    return this.checkoutFormGroup.get('creditCard.nameOnCard');
+  }
+
+  get creditCardNumber(){
+    return this.checkoutFormGroup.get('creditCard.cardNumber');
+  }
+
+  get creditCardSecurityCode(){
+    return this.checkoutFormGroup.get('creditCard.securityCode');
   }
 
   copyShippingAddressToBillingAddress(event: Event){
